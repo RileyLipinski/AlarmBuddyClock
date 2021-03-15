6070178b330d5b5d342d840131ca15bee92d696b
baluttt224 <hannah.balut@gmail.com>
2021-03-15 00:35:03 -0500

    Initial commit

---
+++ app/src/main/java/edu/ust/alarmbuddy/ui/alarms/AlarmsViewModel.java
@@ -0,0 +1,17 @@
+package edu.ust.alarmbuddy.ui.alarms;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AlarmsViewModel extends ViewModel{
+
+    private MutableLiveData<String> mText;
+
+    public AlarmsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Alarm #1");
+    }
+
+    public LiveData<String> getText() { return mText; }
+}
