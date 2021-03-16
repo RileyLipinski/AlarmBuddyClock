59dd1e77e2923546a5065cddc850a230b60623a3
Jacob Jensen <jacob@jacobj.org>
2021-03-16 17:56:16 -0500

    Added grabbing all sounds for a user and started implementing inserting new user into database.  Both need to be tested.

--- Database Designers/RestAPI/restAPI.js
+++ Database Designers/RestAPI/restAPI.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var connection = require('./database');
+var bcrypt = require('bcryptjs');
 
 app.route('/users/:userId')
 //this just take in the UserId and returns all their information from the GoogleDB
@@ -15,9 +16,54 @@ app.route('/users/:userId')
       }
     );
   });
+
+
 app.put('/newUser', (req, res)=>{
+//Need to be able to pull all the different params passed into this one
+  var chosenUsername = req.body.username;
+  //This should be cleartext?
+  var passwordUnhashed = req.body.password;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  var phoneNumber = req.body.phoneNumber;
+  var creationDateTimestamp = Date.now;
+  var birthdate = req.body.birthDate;
+  //need to check to make sure username is unique, check for users with that username, and if there is one, fail the whole process.
+
+
+  var salt = chosenUsername.substring(2,4);
+  var hash = bcrypt.hashSync(passwordUnhashed,salt);
+  console.log(hash);
+  
+  //if username is unique
+  //also need to make sure that their username hits minimum requirements..
+
+  //HASH PASSWORD HERE
+
+
+  //"INSERT INTO users (username, password, first_Name, last_Name, email, phone_Number,creation_Date, birth_Date
+  // VALUES
+  // (?,?,?,?,?,?,?,?)", [chosenUsername,HASHED PASSWORD HERE,firstName,lastName,email,phoneNumber,creationDateTimestamp,birthdate]
+  
 
 });
+
+
+//This is for grabbing sounds for a specific user (needs to be stress tested)
+
+app.route('/sounds/:userId')
+  .get(function(req,res,next){
+    connection.query(
+      "SELECT * FROM alarmbuddy.sounds WHERE soundOwner = ?", req.params.userId,
+      function(error,results,fields){
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+
 app.get('/status', (req, res) => res.send('Working!'));
 
 //Port 8080 for Google App Engine
