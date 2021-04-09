0444a9a1496c47c0a5faf7bc1508c2e9e5a4e577
Jack Reichenbach <jreichen@gustavus.edu>
2021-04-09 10:59:52 -0500

    Login screen updates

--- android/app/src/main/java/edu/ust/alarmbuddy/LoginActivity.java
+++ android/app/src/main/java/edu/ust/alarmbuddy/LoginActivity.java
@@ -7,13 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.lifecycle.ViewModelProviders;
-import edu.ust.alarmbuddy.ui.LoginViewModel;
+import edu.ust.alarmbuddy.ui.login.FailedLoginDialogFragment;
+import edu.ust.alarmbuddy.ui.login.LoginViewModel;
 import edu.ust.alarmbuddy.R;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    int loginAttempts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
         LoginViewModel viewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
-        final Button button = findViewById(R.id.loginButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 // TODO: text comparison not working
@@ -33,13 +35,20 @@ public class LoginActivity extends AppCompatActivity {
                 String stringUsername = username.getText().toString();
                 String stringPassword = password.getText().toString();
                 // if username and password match, "login" to homepage
-                if (stringUsername == "a" && stringPassword == "a") {
+                //Type "android" for username/password to see failed login attempt. Anything else will log you into the app.
+                if (stringUsername.equals("android") && stringPassword.equals("android")  && loginAttempts < 4) {
+                    //loginToHome(v);
+                    FailedLoginDialogFragment dialog = new FailedLoginDialogFragment();
+                    dialog.show(getSupportFragmentManager(), "ABC");
+                }
+                else {
+                    loginAttempts++;
                     loginToHome(v);
                 }
-
-                loginToHome(v);
             }
         });
+
+        //final Button forgotPasswordButton = findViewById(R.id.loginButton)
     }
 
     public void loginToHome(View view) {
