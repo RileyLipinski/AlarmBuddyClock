239f4e324b00065d5c08746ce2e91801ca411d50
lani-lee <55515583+lani-lee@users.noreply.github.com>
2021-04-11 17:48:22 -0500

    Add create account activity

--- android/app/src/main/java/edu/ust/alarmbuddy/LoginActivity.java
+++ android/app/src/main/java/edu/ust/alarmbuddy/LoginActivity.java
@@ -2,11 +2,13 @@ package edu.ust.alarmbuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.lifecycle.ViewModelProviders;
+import edu.ust.alarmbuddy.common.AlarmBuddyHttp;
 import edu.ust.alarmbuddy.ui.login.FailedLoginDialogFragment;
 import edu.ust.alarmbuddy.ui.login.LoginViewModel;
 import edu.ust.alarmbuddy.R;
@@ -24,39 +26,55 @@ public class LoginActivity extends AppCompatActivity {
         LoginViewModel viewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
         final Button loginButton = findViewById(R.id.loginButton);
+        final Button goToCreateAccountButton = findViewById(R.id.goToCreateAccountButton);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                // TODO: text comparison not working
                 // get username/password from input
                 TextView username = findViewById(R.id.textUsername);
                 TextView password = findViewById(R.id.textPassword);
+
                 // convert TextView to strings for comparison
                 String stringUsername = username.getText().toString();
                 String stringPassword = password.getText().toString();
+
                 // if username and password match, "login" to homepage
-                //Type "android" for username/password to see failed login attempt. Anything else will log you into the app.
-                if (stringUsername.equals("android") && stringPassword.equals("android")  && loginAttempts < 4) {
-                    //loginToHome(v);
-                    FailedLoginDialogFragment dialog = new FailedLoginDialogFragment();
-                    dialog.show(getSupportFragmentManager(), "ABC");
+                AlarmBuddyHttp h = new AlarmBuddyHttp();
+                try {
+                    if (h.authenticateLogin(stringUsername, stringPassword) && loginAttempts < 4) {
+                        loginToHome();
+                    } else {
+                        loginAttempts++;
+                        FailedLoginDialogFragment dialog = new FailedLoginDialogFragment();
+                        dialog.show(getSupportFragmentManager(), "TAG");
+                    }
                 }
-                else {
-                    loginAttempts++;
-                    loginToHome(v);
+                catch (Exception e) {
+                    Log.d("TAG", e.toString());
                 }
             }
         });
 
         //final Button forgotPasswordButton = findViewById(R.id.loginButton)
+        // TODO: forgot password action
+
+        goToCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveToCreateAccount();
+            }
+        });
+
     }
 
-    public void loginToHome(View view) {
-        Intent homepage = new Intent(this, MainActivity.class);
-        startActivity(homepage);
+    public void loginToHome() {
+        startActivity(new Intent(this, MainActivity.class));
+
     }
 
-
+    private void moveToCreateAccount() {
+        startActivity(new Intent(this, CreateAccountActivity.class));
+    }
 }
 
 
--- android/app/src/main/java/edu/ust/alarmbuddy/common/AlarmBuddyHttp.java
+++ android/app/src/main/java/edu/ust/alarmbuddy/common/AlarmBuddyHttp.java
@@ -1,12 +1,22 @@
 package edu.ust.alarmbuddy.common;
 
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import android.os.Build;
+import android.util.Log;
+import androidx.annotation.RequiresApi;
+import okhttp3.*;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Tutorial: https://www.journaldev.com/13629/okhttp-android-example-tutorial
  */
+
 public class AlarmBuddyHttp {
 //    private static final String DRIVER = "com.mysql.jdbc.Driver";
 //    private static final String URL = "jdbc:mysql://34.122.120.203";
@@ -15,10 +25,12 @@ public class AlarmBuddyHttp {
 //    private static final String PASS = ";pR$-fM]s4-F?2V%";
 
     private static final String LOCAL_SERVER_URL = "http://10.0.2.2:8080";
-
+    private static final String AB_RESTAPI = "https://alarmbuddy.wm.r.appspot.com/";
+    public static final MediaType QUERYSTRING = MediaType.parse("application/x-www-form-urlencoded");
     private static final OkHttpClient client = new OkHttpClient();
 
     public static void getLatestAlarmSound(Callback c) {
+
         Request r = new Request.Builder()
                 .url(LOCAL_SERVER_URL + "/audio-test")
                 .get()
@@ -26,4 +38,81 @@ public class AlarmBuddyHttp {
         System.out.println("Sending request to " + r.url().toString());
         client.newCall(r).enqueue(c);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static boolean authenticateLogin(String username, String password) throws IOException {
+
+        //build the request
+        String data = "username=" + username + "&password=" + password;
+        URL url = new URL("https://alarmbuddy.wm.r.appspot.com/login");
+        RequestBody body = RequestBody.create(data, QUERYSTRING);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        //execute the request and wait for a response
+        final String[] stringResponse = new String[1];
+        final CountDownLatch latch = new CountDownLatch(1);
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+                latch.countDown();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                stringResponse[0] = response.body().string();
+                latch.countDown();
+            }
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return stringResponse[0].substring(8,12).equals("true") && stringResponse[0] != null;
+
+    }
+
+    public static boolean createUser (String username, String password, String firstName, String lastName,
+                                      String email, String phoneNumber, String birthdate)
+    {
+        String data = String.format("username=%s&password=%s&firstName=%s&lastName=%s&email=%s&phoneNumber=%s&birthDate=%s",
+                username, password, firstName, lastName, email, phoneNumber, birthdate);
+        RequestBody body = RequestBody.create(data, QUERYSTRING);
+        Request request = new Request.Builder()
+                .url("https://alarmbuddy.wm.r.appspot.com/register")
+                .post(body)
+                .build();
+
+
+        //execute the request and wait for a response
+        final String[] stringResponse = new String[1];
+        final CountDownLatch latch = new CountDownLatch(1);
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+                latch.countDown();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                stringResponse[0] = response.body().string();
+                latch.countDown();
+            }
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return stringResponse[0] != null && stringResponse[0].substring(8,12).equals("true");
+
+    }
+
 }
