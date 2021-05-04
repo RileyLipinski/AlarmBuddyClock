571acd53d05ab017fa77ae59b5bf284d3163a6f2
Kegofpad1 <hall1483@stthomas.edu>
2021-05-03 20:44:54 -0500

    Progress on friends page

--- android/app/src/main/java/edu/ust/alarmbuddy/ui/friends/SendRequest.java
+++ android/app/src/main/java/edu/ust/alarmbuddy/ui/friends/SendRequest.java
@@ -7,11 +7,23 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.ust.alarmbuddy.R;
+import edu.ust.alarmbuddy.common.UserData;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /***
  * @author Keghan Halloran
@@ -24,6 +36,7 @@ import edu.ust.alarmbuddy.R;
 public class SendRequest extends AppCompatActivity {
 
 	private Button button;
+	private EditText entry;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +44,77 @@ public class SendRequest extends AppCompatActivity {
 		setContentView(R.layout.activity_send_request);
 
 		ActionBar actionBar = getSupportActionBar();
-		actionBar.setTitle("Friend Request");
+		actionBar.setTitle("Send A Friend Request");
 		actionBar.setDisplayHomeAsUpEnabled(true);
 
 		button = findViewById(R.id.button);
-		button.setOnClickListener(v -> showToast());
+		entry = findViewById(R.id.textUsername);
+		entry.setText(" ");
+		button.setOnClickListener(v -> {
+			try {
+				send();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 
 
 	}
 
-	private void showToast() {
+	private void send() throws InterruptedException {
+		OkHttpClient client = new OkHttpClient();
+
+		String token = "";
+		try {
+			token = UserData.getString(this, "token");
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String username = "";
+		try {
+			username = UserData.getString(this, "username");
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Request request = new Request.Builder()
+				.url("https://alarmbuddy-312620.uc.r.appspot.com/sendRequest/" + username + "/" + entry.getText().toString())
+				.header("Authorization", token)
+				.build();
+
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(@NotNull Call call, @NotNull IOException e) {
+				showToast("Request could not be sent");
+			}
+
+			@Override
+			public void onResponse(@NotNull Call call, @NotNull Response response)
+					throws IOException {
+				if (response.isSuccessful()) {
+					showToast("Request sent");
+
+				} else {
+					showToast("Request could not be sent");
+				}
+			}
+		});
+	}
+
+	private void showToast(String input) {
 		LayoutInflater inflater = getLayoutInflater();
-		View layout = inflater
-			.inflate(R.layout.friend_request_toast, findViewById(R.id.toast_root));
-		Toast toast = new Toast(getApplicationContext());
+		View layout = inflater.inflate(R.layout.friend_request_toast, findViewById(R.id.toast_root));
+
+		TextView text = layout.findViewById(R.id.toast_text);
+		text.setText(input);
+
+		Toast toast = new Toast(this);
 		toast.setGravity(Gravity.CENTER, 0, 0);
-		toast.setDuration(Toast.LENGTH_LONG);
+		toast.setDuration(Toast.LENGTH_SHORT);
 		toast.setView(layout);
 		toast.show();
 	}
